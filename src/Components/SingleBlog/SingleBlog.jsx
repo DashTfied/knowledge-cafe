b21a9bb7c91a0dcef17d3c2f30d03fc4ed316f7f
DashTfied <htanzir13@gmail.com>
2023-03-30 22:24:13 +0600

    published and bookmark

--- src/Components/SingleBlog/SingleBlog.jsx
+++ src/Components/SingleBlog/SingleBlog.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
 const SingleBlog = ({blog}) => {
-    console.log(blog)
+    
     return (
         <div>
             <div className="card w-3/5 bg-base-100 shadow-xl mt-12">
   <figure><img src={blog.cover_img} alt="Shoes" /></figure>
   <div className="card-body">
     <div>
-        <div className='flex gap-6 '>
+        <div className='flex justify-between'>
+            <div className='flex gap-6 '>
             <img className="w-24 rounded-full" src={blog.author_img} alt="" />
         <div className='mt-4'> 
             <h4 className='font-bold text-2xl'>{blog.author_name}</h4>
             <p className='font-thin text-sm'>{blog.published}</p>
+        </div>
+        </div>
+        <div className='flex gap-2'>
+            <p className='mt-8'>{blog.read_time}</p>
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="hover:bg-violet-600 w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+</svg>
+</button>
         </div>
         </div>
     </div>
